acea018df97cdba330a769f95fef6ec8be44982f
anthonyharold67 <93030628+anthonyharold67@users.noreply.github.com>
2023-01-17 23:36:04 +0300

    updated

--- api/tutorial/urls.py
+++ api/tutorial/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import TutorialMVS
-from .views import tutorial_detail, tutorial_list
+# from .views import tutorial_detail, tutorial_list
 
 router=DefaultRouter()
 router.register(r'tutorials',TutorialMVS)
@@ -9,8 +9,8 @@ router.register(r'tutorials',TutorialMVS)
 urlpatterns= router.urls
 
 #fbv
-urlpatterns += [
-    path("tutorialsfbv/",tutorial_list,name="tutorial"),
-    path("tutorialsfbv/<int:id>",tutorial_detail,name="tutorial_detail"),
-]
+# urlpatterns += [
+#     path("tutorialsfbv/",tutorial_list,name="tutorial"),
+#     path("tutorialsfbv/<int:id>",tutorial_detail,name="tutorial_detail"),
+# ]
 
